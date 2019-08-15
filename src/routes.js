1654d95a5484e1efedf0927e49c235a7d2296544
= <davilloaurelio@hotmail.com>
2019-08-14 21:56:03 -0300

    backend finished

--- src/routes.js
+++ src/routes.js
@@ -1,3 +1,11 @@
-server.get("/", (req, res) => {
-    return res.json({ message: `Hello, ${req.query.name}` }); //cria uma rota para "/"
-});
+const express = require("express");
+const routes = express.Router();
+const DevController = require("./controllers/DevControllers");
+const LikeController = require("./controllers/LikeController");
+const DislikeController = require("./controllers/DislikeController");
+
+routes.post("/devs", DevController.store);
+routes.post("/devs/:devId/likes", LikeController.store);
+routes.post("/devs/:devId/dislikes", DislikeController.store);
+routes.get('/devs', DevController.index);
+module.exports = routes;
--- src/server.js
+++ src/server.js
@@ -1,7 +1,15 @@
 const express = require("express"); //importa o express
+const routes = require("./routes");
+const mongoose = require("mongoose");
+const cors = require("cors");
 
 const server = express(); //cria uma const de instancia
-
-
+mongoose.connect(
+  "mongodb+srv://davillo:davillo@cluster0-76pdr.mongodb.net/omnistack?retryWrites=true&w=majority",
+  { useNewUrlParser: true }
+);
+server.use(cors());
+server.use(express.json());
+server.use(routes);
 
 server.listen(3333); //escuta na porta 3333
